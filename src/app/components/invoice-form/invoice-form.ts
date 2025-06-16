9eb1c71fb8a7e40c21f7dfa88039cd68d566e993
Eran Einav <erane.websites@gmail.com>
2025-06-16 16:45:40 +0300

    Refactor InvoiceForm component: implement reactive form with validation, update template for invoice details, and add styles for layout

--- src/app/components/invoice-form/invoice-form.ts
+++ src/app/components/invoice-form/invoice-form.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-invoice-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './invoice-form.html',
-  styleUrl: './invoice-form.scss'
+  styleUrl: './invoice-form.scss',
+  standalone: true
 })
 export class InvoiceForm {
 
+  private _fb = inject(FormBuilder);
+
+  public invoiceForm = this._fb.group({
+    personal: this._fb.group({
+      fullName: ['', [Validators.required]],
+      emailAddress: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.pattern(/^\d{10}$/)]]
+    }),
+    invoice: this._fb.group({
+      number: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+      amount: [1, [Validators.required, Validators.min(1)]],
+      date: [new Date(), [Validators.required]/*, [this.dateValidator]*/],
+    })
+  });
+
+  public onSubmit(): void {
+    console.log(this.invoiceForm.value);
+  }
 }
