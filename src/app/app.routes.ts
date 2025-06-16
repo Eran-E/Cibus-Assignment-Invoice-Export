103f28374b87f89936da9ee034d91749d5411349
Eran Einav <erane.websites@gmail.com>
2025-06-16 15:27:21 +0300

    Update app routes to include InvoiceForm component and redirect for unmatched paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/invoice-form/invoice-form').then(m => m.InvoiceForm)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
