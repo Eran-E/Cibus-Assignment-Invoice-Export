d87858105142e8c6ffecf91e353b1cf3e3eb1c89
Eran Einav <erane.websites@gmail.com>
2025-06-18 15:52:53 +0300

    Refactor header component to implement dark mode functionality: add OnInit lifecycle hook, load dark mode preference from localStorage, and update theme accordingly. Simplify dark mode toggle logic and ensure proper theme setting based on user preference.

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -9,19 +9,31 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './header.scss',
   standalone: true
 })
-export class Header {
+export class Header implements OnInit {
 
-  public isDarkMode = signal<boolean>(localStorage.getItem('darkMode') === 'true' || true);
+  public isDarkMode = signal<boolean>(true);
+
+  ngOnInit(): void {
+    this._loadDarkMode();
+  }
+
+  private _setTheme(): void {
+    if (this.isDarkMode()) {
+      document.documentElement.style.setProperty('color-scheme', 'dark');
+    } else {
+      document.documentElement.style.setProperty('color-scheme', 'light');
+    }
+  }
+
+  private _loadDarkMode(): void {
+    const darkMode = localStorage.getItem('darkMode');
+    this.isDarkMode.set(darkMode === 'true');
+    this._setTheme();
+  }
 
   public toggleDarkMode(): void {
-
-    if (this.isDarkMode()) {
-      document.documentElement.style.setProperty('color-scheme', 'light');
-    } else {
-      document.documentElement.style.setProperty('color-scheme', 'dark');
-    }
-
     this.isDarkMode.set(!this.isDarkMode());
+    this._setTheme();
     localStorage.setItem('darkMode', this.isDarkMode().toString());
   }
   
